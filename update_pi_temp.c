18ad0e5ebcc64a356f65c533486ceab16291dfbb
schnitz81 <k.quick81@gmail.com>
2013-10-18 00:16:46 +0200

    Update update_pi_temp.c

--- update_pi_temp.c
+++ update_pi_temp.c
@@ -5,43 +5,38 @@
 #include <stdbool.h>
 #include <string.h>
 
-
 char temp[4];
-
 
 void gettemp()
 {
-  FILE *p;
-  char buff[20];
-  p = popen("/opt/vc/bin/vcgencmd measure_temp", "r");  
-  if (! p)
-    {
-    perror("Failed to run command\n");
-    exit(0);
+    FILE *p;
+    char buff[20];
+    p = popen("/opt/vc/bin/vcgencmd measure_temp", "r");  
+    if (! p){
+        perror("Failed to run command\n");
+        exit(0);
     }
-  fgets( buff, sizeof buff, p);
-  pclose(p);
-  if( isdigit(buff[0]) )
-    memcpy(&temp, &buff,4);
-  else
-    memcpy(&temp, &buff[5],4);
+    fgets( buff, sizeof buff, p );
+    pclose(p);
+    if( isdigit(buff[0]) )
+        memcpy(&temp, &buff,4);
+    else
+        memcpy(&temp, &buff[5],4);
 }
 
 void writetemp()
 {
-FILE *file;
-file = fopen("/run/temp","w");
-  fprintf(file,temp);
-  fclose(file);
-  //return 0;
+    FILE *file;
+    file = fopen("/run/temp","w");
+    fprintf(file,temp);
+    fclose(file);
 }
 
 int main()
 {
-  while (true)
-  {
-    gettemp();
-    writetemp();
-    sleep(5);
-  }  
+    while (true){
+        gettemp();
+        writetemp();
+        sleep(5);
+    }  
 }
